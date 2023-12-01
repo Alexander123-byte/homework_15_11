7adbaa6af326dbfa210b94dc531966eb860e545d
Alexander <san.juckov2017@yandex.ru>
2023-12-01 13:01:36 +0300

    Добавлен файл str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def to_uppercase(string):
+    """Строка с заглавными буквами"""
     return string.upper()
+
+
+def capitalize_first_letters(string):
+    """Строка с первыми заглавными буквами"""
+    return ' '.join(word.capitalize() for word in string.split())
